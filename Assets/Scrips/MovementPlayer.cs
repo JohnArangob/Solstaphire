cd10cbd7641ad9938a32a762797047ddabb5ea69
John Arango Buitrago <john.arango496@pascualbravo.edu.co>
2024-02-12 17:54:36 -0500

    Otros ajustes
    Ajustes con el ignoreCollision (se esta intentado)

--- Assets/Scrips/MovementPlayer.cs
+++ Assets/Scrips/MovementPlayer.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class MovementPlayer : MonoBehaviour
 {
+    public Collider2D playes;
+    private Collider2D others = FindObjectOfType<Collider2D>();
+
     public GameObject character;
+
     public float Speed;
     private float MinSpeed = 11f;
     public Rigidbody2D rb;
@@ -21,11 +26,15 @@ public class MovementPlayer : MonoBehaviour
     private bool canDash = true;
     private bool Jump = true;
     private float movement;
+    private bool ignore = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         gravedad = rb.gravityScale;
+
+
+
     }
 
     private void Update()
@@ -37,21 +46,25 @@ public class MovementPlayer : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
         {
             StartCoroutine(invulnerableRight());
+            StartCoroutine(invulnerable());
         }
         else if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
         {
             StartCoroutine(invulnerableLeft());
+            StartCoroutine(invulnerable());
+
         }
-        else if (Input.GetKeyDown(KeyCode.Space))   
+        else if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(Salto());
+            
         }
-       
+
     }
 
     IEnumerator invulnerableRight()
     {
-        if  (canDash)
+        if (canDash)
 
         {
             dashing = true;
@@ -76,7 +89,7 @@ public class MovementPlayer : MonoBehaviour
             canDash = false;
             rb.gravityScale = 0f;
             print("Sprint");
-            rb.velocity = new Vector2(movement*dashForce,0f);
+            rb.velocity = new Vector2(movement * dashForce, 0f);
             yield return new WaitForSeconds(dashTime);
             dashing = false;
             rb.gravityScale = gravedad;
@@ -84,7 +97,7 @@ public class MovementPlayer : MonoBehaviour
             canDash = true;
 
         }
-        
+
     }
     IEnumerator Salto()
     {
@@ -99,6 +112,16 @@ public class MovementPlayer : MonoBehaviour
             yield return new WaitForSeconds(timeJump);
             Jump = true;
         }
-        
+
+    }
+    IEnumerator invulnerable()
+    {
+        if (ignore = false)
+        {
+            ignore = true;
+            Physics2D.IgnoreCollision(playes, others, ignore);
+            ignore = false;
+            yield return new WaitForSeconds(timeDash);
+        }
     }
 }
