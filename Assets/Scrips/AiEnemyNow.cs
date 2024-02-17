8bcf9cda7b0608529426f21d995dddbe41ac3a69
John Arango Buitrago <john.arango496@pascualbravo.edu.co>
2024-02-17 14:57:32 -0500

    Revert "Ultima version"
    This reverts commit c0220cbe452f458e23a9d52cce9c9215e7bbe6ce.

--- Assets/Scrips/AiEnemyNow.cs
+++ Assets/Scrips/AiEnemyNow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AiEnemyNow : MonoBehaviour
+public class AIEnemyNow : MonoBehaviour
 {
     public Estados estado;
     public float speed;
@@ -12,9 +12,7 @@ public class AiEnemyNow : MonoBehaviour
     private float _distancia;
     public bool vivo;
     private Rigidbody2D _rbEnemy;
-    public float direction = 1;
-
-    public float daño = 40f;
+    public float direction=1;
 
 
     void Start()
@@ -23,7 +21,7 @@ public class AiEnemyNow : MonoBehaviour
         StartCoroutine(CalcularDistancia());
     }
 
-
+    
     void Update()
     {
         switch (estado)
@@ -47,14 +45,14 @@ public class AiEnemyNow : MonoBehaviour
     void EstadoSeguir()
     {
         float directionX = Mathf.Clamp(PlayerControl.singleton.transform.position.x - _rbEnemy.transform.position.x, -1, 1);
-        Vector2 direction = new Vector2(directionX * Mathf.Abs(speed), _rbEnemy.velocity.y);
+        Vector2 direction = new Vector2(directionX * Mathf.Abs(speed) , _rbEnemy.velocity.y);
 
-        _rbEnemy.velocity = direction;
+        _rbEnemy.velocity = direction ;
         if (_distancia < distanciaAtacar)
         {
             CambiarEstado(Estados.atacar);
         }
-        else if (_distancia > distanciaEscapar)
+        else if (_distancia>distanciaEscapar)
         {
             CambiarEstado(Estados.patrullar);
         }
@@ -62,8 +60,8 @@ public class AiEnemyNow : MonoBehaviour
     void EstadoAtacar()
     {
         //Causar daño al jugador
-        Atacar();
-        if (_distancia > distanciaAtacar + 0.5f)
+
+        if (_distancia>distanciaAtacar + 0.5f)
         {
             CambiarEstado(Estados.seguir);
         }
@@ -71,7 +69,7 @@ public class AiEnemyNow : MonoBehaviour
     void EstadoPatrullar()
     {
         _rbEnemy.velocity = new Vector2(speed * direction, _rbEnemy.velocity.y * Time.deltaTime);
-        if (_distancia < distanciaSeguir)
+        if (_distancia<distanciaSeguir)
         {
             CambiarEstado(Estados.seguir);
         }
@@ -82,7 +80,7 @@ public class AiEnemyNow : MonoBehaviour
     }
     void CambiarEstado(Estados estados)
     {
-
+        
         estado = estados;
         //Es como el start de cada estado
         switch (estado)
@@ -117,7 +115,7 @@ public class AiEnemyNow : MonoBehaviour
         }
         if (other.gameObject.tag == "PJ")
         {
-            //perseguirPlayer = true;
+           //perseguirPlayer = true;
         }
     }
     void Colision()
@@ -135,13 +133,6 @@ public class AiEnemyNow : MonoBehaviour
         Gizmos.DrawWireSphere(this.transform.position, distanciaSeguir);
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(this.transform.position, distanciaEscapar);
-    } 
-    public void Atacar()
-    {
-        if (_distancia < distanciaAtacar)
-        {
-            PlayerControl.singleton.vida.CausarDaño(daño);
-        }
     }
 }
 
