a0ba1e99afa1589145f6b8a3e661267e9def1e6b
John Arango Buitrago <john.arango496@pascualbravo.edu.co>
2024-02-18 16:04:30 -0500

    18/02/2024-4:04PM

--- Assets/Scrips/AiEnemyNow.cs
+++ Assets/Scrips/AiEnemyNow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIEnemyNow : MonoBehaviour
+public class AiEnemyNow : MonoBehaviour
 {
     public Estados estado;
     public float speed;
@@ -45,9 +45,9 @@ public class AIEnemyNow : MonoBehaviour
     void EstadoSeguir()
     {
         float directionX = Mathf.Clamp(PlayerControl.singleton.transform.position.x - _rbEnemy.transform.position.x, -1, 1);
-        Vector2 direction = new Vector2(directionX * Mathf.Abs(speed) , _rbEnemy.velocity.y);
+        Vector2 direction = new Vector2(directionX * Mathf.Abs(speed), _rbEnemy.velocity.y);
 
-        _rbEnemy.velocity = direction ;
+        _rbEnemy.velocity = direction;
         if (_distancia < distanciaAtacar)
         {
             CambiarEstado(Estados.atacar);
@@ -60,7 +60,6 @@ public class AIEnemyNow : MonoBehaviour
     void EstadoAtacar()
     {
         //Causar daño al jugador
-
         if (_distancia>distanciaAtacar + 0.5f)
         {
             CambiarEstado(Estados.seguir);
@@ -68,7 +67,7 @@ public class AIEnemyNow : MonoBehaviour
     }
     void EstadoPatrullar()
     {
-        _rbEnemy.velocity = new Vector2(speed * direction, _rbEnemy.velocity.y * Time.deltaTime);
+        _rbEnemy.velocity = new Vector2(speed * direction, _rbEnemy.velocity.y);
         if (_distancia<distanciaSeguir)
         {
             CambiarEstado(Estados.seguir);
